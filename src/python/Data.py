686db0792afccd879a680349e06d1cc191363b10
Antonio <a.linares@cern.ch>
2023-07-25 01:21:52 -0500

    New API Entry with history of primary dataset configuration

--- src/python/Data.py
+++ src/python/Data.py
@@ -11,7 +11,7 @@ from T0WmaDataSvc.DataPromptRecoStatus import *
 from T0WmaDataSvc.DataDatasetLocked import *
 from T0WmaDataSvc.DataRepackStats import *
 from T0WmaDataSvc.DataRunStreamSkippedLumis import *
-
+from T0WmaDataSvc.DataPrimaryDatasetConfig import *
 
 class Data(DatabaseRESTApi):
   """Server object for REST data access API."""
