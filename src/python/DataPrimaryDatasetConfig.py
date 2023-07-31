0016c9a9fa53e9caae48fdd3297709162bfd6d64
Antonio <a.linares@cern.ch>
2023-07-31 08:10:21 -0500

    DataPrimaryDatasetConfig update

--- src/python/DataPrimaryDatasetConfig.py
+++ src/python/DataPrimaryDatasetConfig.py
@@ -20,10 +20,10 @@ class PrimaryDatasetConfig(RESTEntity):
     :returns: PrimaryDataset, Acquisition era, minimum run, maximum run, CMSSW, PhysicsSkim, DqmSeq, GlobalTag"""
 
     sql = """
-            SELECT reco_config.primds primds, run_config.acq_era acq_era, MIN(run_config.run) min_run, MAX(run_config.run) max_run, reco_config.cmssw cmssw, reco_config.global_tag global_tag, reco_config.physics_skim physics_skim, reco_config.dqm_seq dqm_seq
+            SELECT reco_config.primds primds, MIN(run_config.run) min_run, MAX(run_config.run) max_run, reco_config.cmssw cmssw, reco_config.global_tag global_tag, reco_config.physics_skim physics_skim, reco_config.dqm_seq dqm_seq, run_config.acq_era acq_era
             FROM reco_config
             JOIN run_config ON run_config.run = reco_config.run
-            WHERE primds = :primary_dataset
+            WHERE primds = :primds
             GROUP BY run_config.acq_era, reco_config.primds, reco_config.cmssw,  reco_config.global_tag, reco_config.physics_skim, reco_config.dqm_seq
             ORDER BY MIN(run_config.run) desc, MAX(run_config.run) desc
             """
