e2a0d9544c18b2867f27211bee4fce6be7130c30
Antonio <a.linares@cern.ch>
2023-10-09 11:12:39 -0500

    Update reco and express config entry points to enable processing version

--- src/python/Regexps.py
+++ src/python/Regexps.py
@@ -4,8 +4,8 @@ import re
 
 RX_RUN = re.compile(r"^[1-9][0-9]{1,6}$")
 RX_STREAM = re.compile(r"[A-Z][0-9a-zA-Z]+")
-RX_PRIMARY_DATASET = re.compile(r"[A-Z][0-9a-zA-Z]+")
-RX_SCENARIO = re.compile(r"[a-zA-Z][0-9a-zA-Z]+")
+RX_PRIMARY_DATASET = re.compile(r"[0-9_a-zA-Z]+")
+RX_SCENARIO = re.compile(r"[0-9_a-zA-Z]+")
 RX_ERA = re.compile(r"[0-9_a-zA-Z]+")
 RX_EXPRESS_GLOBAL_TAG = re.compile(r"[0-9_a-zA-Z]+")
 RX_PROMPT_GLOBAL_TAG = re.compile(r"[0-9_a-zA-Z]+")
