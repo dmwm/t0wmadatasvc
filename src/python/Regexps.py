7ba07874108cda64830df8adef1c8b137ff81a60
Antonio Linares Sancho <132582167+LinaresToine@users.noreply.github.com>
2023-10-13 10:12:52 -0500

    Update Regexps.py
    Add wildcard use to stream queries

--- src/python/Regexps.py
+++ src/python/Regexps.py
@@ -3,7 +3,7 @@ import re
 #: Regular expression for Tier0 Run ID.
 
 RX_RUN = re.compile(r"^[1-9][0-9]{1,6}$")
-RX_STREAM = re.compile(r"[A-Z][0-9a-zA-Z]+")
+RX_STREAM = re.compile(r"[0-9_a-zA-Z]+")
 RX_PRIMARY_DATASET = re.compile(r"[0-9_a-zA-Z]+")
 RX_SCENARIO = re.compile(r"[0-9_a-zA-Z]+")
 RX_ERA = re.compile(r"[0-9_a-zA-Z]+")
